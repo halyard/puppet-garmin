688837ef890036ada311539281800f20f165bb45
Les Aker <me@lesaker.org>
2025-07-20 23:18:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+puppet-garmin
+===========
+
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/garmin.svg)](https://forge.puppetlabs.com/halyard/garmin)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/halyard/puppet-garmin/build.yml?branch=main)](https://github.com/halyard/puppet-garmin/actions)
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
+
+Module to configure [Garmin importer](https://github.com/arpanghosh8453/garmin-grafana)
+
+## Usage
+
+```puppet
+include garmin
+```
+
+## License
+
+garmin is released under the MIT License. See the bundled LICENSE file for details.
+
---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+# @summary Configure Grafana importer instance
+#
+# @param token_dir sets the location for persistent token storage
+# @param influxdb_host sets the influx host
+# @param influxdb_username sets the influx username
+# @param influxdb_password sets the influx password
+# @param influxdb_database sets the influx database name
+# @param influxdb_port sets the port to connect to influx
+# @param container_ip sets the address of the Docker container
+class garmin (
+  String $token_dir,
+  String $influxdb_host,
+  String $influxdb_username,
+  String $influxdb_password,
+  String $influxdb_database = 'garmin',
+  String $influxdb_port = '8086',
+  String $container_ip = '172.17.0.3',
+) {
+
+  file { $token_dir:
+    ensure => directory,
+  }
+
+  -> docker::container { 'garmin':
+    image => 'thisisarpanghosh/garmin-fetch-data:latest',
+    args  => [
+      "--ip ${container_ip}",
+      "-v ${token_dir}:/home/appuser/.garminconnect",
+      "-e INFLUXDB_HOST=${}",
+      "-e INFLUXDB_PORT=${}",
+      "-e INFLUXDB_USERNAME=${}",
+      "-e INFLUXDB_PASSWORD=${}",
+      "-e INFLUXDB_DATABASE=${}",
+    ],
+    cmd   => '',
+  }
+}
